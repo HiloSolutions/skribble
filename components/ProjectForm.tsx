fc9b667b3b2532539abdcc16049af6c565905225
Lauren Johnston <hilotraining@outlook.com>
2023-08-12 15:32:56 -0600

    made image for project form component

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+"use client"
 
-const ProjectForm = () => {
+import Image from "next/image"
+import { SessionInterface } from "@/common.types"
+
+type Props = {
+  type: string,
+  session: SessionInterface
+}
+const ProjectForm = ({ type, session }: Props) => {
+
+  const form = {
+    image: '',
+  };
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+
+  };
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+
+  };
+
   return (
-    <div>ProjectForm</div>
+    <form
+      onSubmit={handleFormSubmit}
+      className="flexStart form"
+    >
+      <div className="flexStart form_image-container">
+        <label htmlForm="poster" className="flexCenter form_image-label">
+          {!form.image && 'Choose a cover for your project'}
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          required={type === 'create'}
+          className="form_image-input"
+          onChange={handleChangeImage}
+        />
+        {form.image &&
+          <Image
+            src={form.image}
+            className="sm:p-10 object-container z-20"
+            alt="Project Poster"
+            fill
+          />
+        }
+      </div>
+    </form>
   )
 }
 
